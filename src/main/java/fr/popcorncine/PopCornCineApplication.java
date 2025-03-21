23dc6ac56a4f42161c9fabcebfc91621b2b01853
Ferreira-RodriguesD <david.ferreira-rodrigues@fms-ea.com>
2025-03-21 11:30:09 +0100

    Initial PopCornCine App backend commit

---
+++ src/main/java/fr/popcorncine/PopCornCineApplication.java
@@ -0,0 +1,13 @@
+package fr.popcorncine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopCornCineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopCornCineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/popcorncine/PopCornCineApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.popcorncine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopCornCineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
